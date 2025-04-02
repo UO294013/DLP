45978437d4a63f00e62c07bbf8bd6a8c212ea9c7
UO294013 <uo294013@uniovi.es>
2025-04-02 11:50:51 +0200

    IdentificationVisitor fix?

--- src/visitor/IdentificationVisitor.java
+++ src/visitor/IdentificationVisitor.java
@@ -14,7 +14,7 @@ public class IdentificationVisitor extends AbstractVisitor<Void, Void> {
     @Override
     public Void visit(FunctionCall f, Void paramType) {
         if (symbolTable.find(f.getFunctionName().getName()) == null) {
-            ErrorType error = new ErrorType("Error: Function '" + f.getFunctionName().getName() +
+            new ErrorType("Error: Function '" + f.getFunctionName().getName() +
                     "' can not be called (It is not defined or accessible from current scope)", f);
         }
         // super.visit(f, paramType); // Descomentar esta línea lanza también error por Variable
@@ -24,7 +24,7 @@ public class IdentificationVisitor extends AbstractVisitor<Void, Void> {
     @Override
     public Void visit(Variable v, Void paramType) {
         if (symbolTable.find(v.getName()) == null) {
-            ErrorType error = new ErrorType("Error: Variable '" + v.getName() +
+            new ErrorType("Error: Variable '" + v.getName() +
                     "' can not be accessed from current scope (Maybe it is not defined)", v);
         }
         super.visit(v, paramType);
@@ -36,7 +36,7 @@ public class IdentificationVisitor extends AbstractVisitor<Void, Void> {
     @Override
     public Void visit(FunctionDefinition f, Void paramType) {
         if (!symbolTable.insert(f)) {
-            ErrorType error = new ErrorType("Error: Function '" + f.getName() +
+            new ErrorType("Error: Function '" + f.getName() +
                     "' could not be defined! (there is already a Definition with the same name in the same scope)", f);
         }
         symbolTable.set();
@@ -48,12 +48,10 @@ public class IdentificationVisitor extends AbstractVisitor<Void, Void> {
     @Override
     public Void visit(VariableDefinition v, Void paramType) {
         if (!symbolTable.insert(v)) {
-            ErrorType error = new ErrorType("Error: Variable '" + v.getName() +
+            new ErrorType("Error: Variable '" + v.getName() +
                     "' could not be defined! (there is already a Definition with the same name in the same scope)", v);
         }
-        symbolTable.set();
         super.visit(v, paramType);
-        symbolTable.reset();
         return null;
     }
 }
